f6848e21abdbb5c3347330664c767520c9a7e4fd
Cedric Chee <cedric@invictusbyte.com>
2020-02-11 11:14:20 +0800

    Update code samples

--- examples/basic.js
+++ examples/basic.js
@@ -1,8 +1,8 @@
 const rnnoise = require("../.");
 
 const denoisedBufLength = rnnoise.suppress(
-  "/home/cedric/Downloads/temp_01/babble_15dB.wav",
-  "/home/cedric/Downloads/temp_01/babble_15dB_dn.wav"
+  "babble_15dB.wav",
+  "babble_15dB_dn.wav"
 );
 
 console.log(`Denoised buffer length: ${denoisedBufLength} bytes`);
